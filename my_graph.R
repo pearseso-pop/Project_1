23bce12aaf9923b6517030489599ee65fdcf73fd
pearseso-pop <pearseso@vcu.edu>
2026-01-28 13:16:11 -0500

    changing back to mpg and adding transmission type

--- my_graph.R
+++ my_graph.R
@@ -2,12 +2,12 @@
 library(ggplot2)
 
 #create visual for mtcars
-ggplot(mtcars, aes(x = factor(cyl), y = wt)) +
-  geom_boxplot(fill = "purple") +
-  geom_jitter(width = 0.1, alpha = 0.6) +
+ggplot(mtcars, aes(x = factor(cyl), y = mpg, color = factor(am))) +
+  geom_jitter(width = 0.1, size = 3) +
   labs(
-    title = "Weight by Number of Cylinders",
+    title = "MPG vs Cylinders by Transmission",
     x = "Cylinders",
-    y = "Weight (Tons)"
+    y = "MPG",
+    color = "Transmission (0=Auto,1=Manual)"
   ) +
   theme_minimal()
